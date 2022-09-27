f161d9d257a9e6b53f68b47746a5b1e2aaa85f15
Aivenest <aivenest@gmail.com>
2022-09-27 14:30:19 +0500

    Exercise02 ver.1

---
+++ Exercise02/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите X");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Y");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x>0&&y>0)
+{
+    Console.WriteLine("I четверть");
+}
+if (x>0&&y<0)
+{
+    Console.WriteLine("IV четверть");
+}
+if (x<0&&y<0)
+{
+    Console.WriteLine("III четверть");
+}
+if (x<0&&y>0)
+{
+    Console.WriteLine("II четверть");
+}
