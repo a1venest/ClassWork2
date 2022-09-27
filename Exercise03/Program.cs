83b0df43e50b01934cf875da36cb1f02ab29e968
Aivenest <aivenest@gmail.com>
2022-09-27 17:20:02 +0500

    Правки

--- Exercise03/Program.cs
+++ Exercise03/Program.cs
@@ -5,17 +5,17 @@
 }
 double CoordinatPoint(double x1, double y1, double x2, double y2)
 {
-    double CP1(double x1, double y1)
+    double CP1(double x1, double x2)
     {
-        return Math.Pow((x1 + y1), 2);
+        return Math.Pow((x2 - y1), 2);
     }
 
-    double CP2(double x2, double y2)
+    double CP2(double y1, double y2)
     {
-        return Math.Pow((x2 + y2), 2);
+        return Math.Pow((y2 - y1), 2);
     }
 
-    return Math.Sqrt((CP1(x1,y1)) + (CP2(x2,y2)));
+    return Math.Sqrt((CP1(x1,x2)) + (CP2(y1,y2)));
 }
 double x1 = GetNumber("x1");
 double y1 = GetNumber("y1");
