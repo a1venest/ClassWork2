b5bf7a448df88ee9384d94c0bd5d141501351824
Aivenest <aivenest@gmail.com>
2022-09-27 13:29:41 +0500

    Ex01 compleated

--- Exercise01/Program.cs
+++ Exercise01/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void Div(int number)
+{
+    if (number%7==0&&number%23==0)
+    {
+        Console.WriteLine("Кратно.");
+    }
+    else
+    {
+        Console.WriteLine("Не кратно.");
+    }
+}
+
+Div(number);
