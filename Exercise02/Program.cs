b44b5cbb2b27532ddae37be52c328a8d47f03030
Aivenest <aivenest@gmail.com>
2022-09-27 15:30:49 +0500

    Ex02 Ver.2

--- Exercise02/Program.cs
+++ Exercise02/Program.cs
@@ -1,21 +1,53 @@
-﻿Console.WriteLine("Введите X");
-int x = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите Y");
-int y = Convert.ToInt32(Console.ReadLine());
+﻿// Console.WriteLine("Введите X");
+// int x = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите Y");
+// int y = Convert.ToInt32(Console.ReadLine());
 
-if (x>0&&y>0)
+// if (x>0&&y>0)
+// {
+//     Console.WriteLine("I четверть");
+// }
+// if (x>0&&y<0)
+// {
+//     Console.WriteLine("IV четверть");
+// }
+// if (x<0&&y<0)
+// {
+//     Console.WriteLine("III четверть");
+// }
+// if (x<0&&y>0)
+// {
+//     Console.WriteLine("II четверть");
+// }
+
+double GetNumber(string name)
 {
-    Console.WriteLine("I четверть");
+    Console.WriteLine("Введите значение " + name);
+    return Convert.ToDouble(Console.ReadLine());
 }
-if (x>0&&y<0)
+
+
+
+int GetNumChetv(double x, double y)
 {
-    Console.WriteLine("IV четверть");
+    if (x > 0 && y > 0)
+
+        return 1;
+
+    else
+    if (x > 0 && y < 0)
+
+        return 4;
+
+    else
+    if (x < 0 && y < 0)
+
+        return 3;
+
+    else return 2;
+
 }
-if (x<0&&y<0)
-{
-    Console.WriteLine("III четверть");
-}
-if (x<0&&y>0)
-{
-    Console.WriteLine("II четверть");
-}
+double x = GetNumber("x");
+double y = GetNumber("y");
+
+Console.WriteLine(GetNumChetv(x, y) + " четверть.");
