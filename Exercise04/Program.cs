07953be74d695c455c2b456ab628cc3cfe0966f8
Aivenest <aivenest@gmail.com>
2022-09-27 16:55:01 +0500

    Ex04 Ver1

---
+++ Exercise04/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+
+Console.WriteLine("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+void PowN(int N)
+{
+    int i = 1;
+    while (i <= N)
+    {
+        Console.WriteLine(Math.Pow(i, 2));
+        i++;
+    }
+}
+
+PowN(N);
