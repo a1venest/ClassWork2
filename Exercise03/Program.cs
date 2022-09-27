da4e79a2cfd5d42275f24fd1ab42d53f850388b8
Aivenest <aivenest@gmail.com>
2022-09-27 16:33:05 +0500

    Ex03 ver.1

---
+++ Exercise03/Program.cs
@@ -0,0 +1,25 @@
+﻿double GetNumber(string name)
+{
+    Console.WriteLine("Введите " + name);
+    return Convert.ToDouble(Console.ReadLine());
+}
+double CoordinatPoint(double x1, double y1, double x2, double y2)
+{
+    double CP1(double x1, double y1)
+    {
+        return Math.Pow((x1 + y1), 2);
+    }
+
+    double CP2(double x2, double y2)
+    {
+        return Math.Pow((x2 + y2), 2);
+    }
+
+    return Math.Sqrt((CP1(x1,y1)) + (CP2(x2,y2)));
+}
+double x1 = GetNumber("x1");
+double y1 = GetNumber("y1");
+double x2 = GetNumber("x2");
+double y2 = GetNumber("y2");
+
+Console.WriteLine("Координаты точки: " + CoordinatPoint(x1, y1, x2, y2));
